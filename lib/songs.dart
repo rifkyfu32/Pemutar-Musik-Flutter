afff524fb36074c1fec1bc729a5efd5259833bf4
rifkyfu32 <rifky2fuady@gmail.com>
2019-03-16 01:56:10 +0700

    push project

---
+++ lib/songs.dart
@@ -0,0 +1,123 @@
+import 'package:meta/meta.dart';
+
+final demoPlaylist = new DemoPlaylist(
+  songs: [
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/151906119/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://rifkyfu32.github.io/img/profil-300.jpg',
+      songTitle: 'Nocturne Op.9 - No.2',
+      artist: 'Chopin',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/52910579/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/avatars-000018621313-4f0n4a-t200x200.jpg',
+      songTitle: 'Romantic Saxophone',
+      artist: 'Kenny G',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/142334780/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/artworks-000075361684-gzr4u4-t200x200.jpg',
+      songTitle: 'Hey Jude',
+      artist: 'The Beatles',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/92389400/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/artworks-000145200947-x75uby-t200x200.jpg',
+      songTitle: 'The Sound of Silence',
+      artist: 'Kenny G',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/81788555/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/artworks-000075361684-gzr4u4-t200x200.jpg',
+      songTitle: 'You Are Not Alone',
+      artist: 'Michael Jackson',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/70363761/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/artworks-000082903283-8pdzbr-t200x200.jpg',
+      songTitle: 'Forever in Love',
+      artist: 'Kenny G',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/142102804/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/artworks-000075004619-97ukj0-t120x120.jpg',
+      songTitle: 'All Of Me',
+      artist: 'John Legend',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/64877255/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/artworks-000032921529-nheeom-t200x200.jpg',
+      songTitle: 'The Prayer',
+      artist: 'Celine Dion',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/434370309/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/avatars-000312484264-af28qp-t200x200.jpg',
+      songTitle: 'Perfect Saxophone',
+      artist: 'Simon More',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/188665880/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/artworks-000105087912-2ggpah-t200x200.jpg',
+      songTitle: 'Thinking Out Loud',
+      artist: 'Ed Sheeran',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/114946791/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/avatars-000057061959-dhszyq-t200x200.jpg',
+      songTitle: 'Piano Saxophone',
+      artist: 'Tidak Dikenal',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/231465078/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/artworks-000075361684-gzr4u4-t200x200.jpg',
+      songTitle: 'Kangen',
+      artist: 'Dewa 19',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/142702132/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/artworks-000075361684-gzr4u4-t200x200.jpg',
+      songTitle: 'Sewindu',
+      artist: 'Tulus',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/173607230/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/artworks-000075361684-gzr4u4-t200x200.jpg',
+      songTitle: 'LDR',
+      artist: 'Raisa',
+    ),
+    new DemoSong(
+      audioUrl: 'https://api.soundcloud.com/tracks/352198709/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P',
+      albumArtUrl: 'https://i1.sndcdn.com/artworks-000252251471-xp4vu9-t200x200.jpg',
+      songTitle: 'Relaxation Saxophone',
+      artist: 'Soft Jazz',
+    ),
+    
+  ],
+);
+
+class DemoPlaylist {
+
+  final List<DemoSong> songs;
+
+  DemoPlaylist({
+    @required this.songs,
+  });
+
+}
+
+class DemoSong {
+
+  final String audioUrl;
+  final String albumArtUrl;
+  final String songTitle;
+  final String artist;
+
+  DemoSong({
+    @required this.audioUrl,
+    @required this.albumArtUrl,
+    @required this.songTitle,
+    @required this.artist,
+  });
+
+}
